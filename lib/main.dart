95fcfbb6f40c895004b027b351f7959e3d5e7322
mhaseeb1991 <mohammadhaseeb1991@gmail.com>
2023-10-30 20:01:31 +0500

    initial commit: Counter App

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'app/counter_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/screens/counter_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return buildMainWidget();
+  }
+
+  Center buildMainWidget() {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text("You have pushed the button this many time"),
+          const SizedBox(height: 10),
+          Text(counter.toString()),
+          const SizedBox(height: 10),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  counter++;
+                });
+              },
+              child: const Text(" + ")),
+          const SizedBox(height: 10),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  counter = 0;
+                });
+              },
+              child: const Text("Reset Counter")),
+          const SizedBox(height: 10),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  counter--;
+                });
+              },
+              child: const Text(" - "))
+        ],
+      ),
+    );
+  }
+}
